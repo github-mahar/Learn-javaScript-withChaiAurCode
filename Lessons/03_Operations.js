2fdbff533a0f7c8420e6b0c3c9f52c451659bf5e
Mahar GM <144817379+github-mahar@users.noreply.github.com>
2025-10-10 22:30:36 +0500

    updated operation.js (< ECMA script)

---
+++ Lessons/03_Operations.js
@@ -0,0 +1,34 @@
+let value = 10;
+let negValue = -value;
+
+// console.log(negValue); // -10
+// Unary negation operator (-) changes the sign of the number
+
+
+
+// console.log(2+2); // 4
+// console.log(2-2); // 0
+// console.log(2*2); // 4
+// console.log(2**2); // 4
+// "**" is the exponentiation operator which raises the base (2) to the power of the exponent (2)
+// console.log(2**3); // 8
+// console.log(2/2); // 1
+// console.log(2%2); // 0
+// % is the modulus operator which gives the remainder of the division
+
+let str1 = "Hello ";
+let str2 = "World!";
+
+
+let str3 = str1 + str2; // String concatenation using + operator
+// console.log(str3); // Hello World!
+
+
+console.log(1 + "2"); // "12"
+console.log("1" + 2); // "12"
+
+console.log(1 + 2 + "3"); // "33"
+console.log("1" + 2 + 3); // "123"
+
+console.log(1 + "2" + 3); // "123"
+console.log("1" + (2 + 3)); // "15"
