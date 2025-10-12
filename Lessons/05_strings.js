86373a527acda578f8a15ade75bdc9cdf9251624
Mahar GM <144817379+github-mahar@users.noreply.github.com>
2025-10-12 06:07:54 +0500

    updated string.js

---
+++ Lessons/05_strings.js
@@ -0,0 +1,6 @@
+const name = "Mahar GM"; // String
+const repoCount = 33; // Number
+
+// console.log(name + repoCount + " repositories"); // Concatenation (not recommended way outdated way)
+
+console.log(`${name} ${repoCount} repositories`); // Template Literal (recommended way modern way)
