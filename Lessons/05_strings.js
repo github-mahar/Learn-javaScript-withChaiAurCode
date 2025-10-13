dc3ca1a9854ea7b804aa45ab960f5d10cdcc35ff
Mahar GM <144817379+github-mahar@users.noreply.github.com>
2025-10-13 22:20:01 +0500

    string object

--- Lessons/05_strings.js
+++ Lessons/05_strings.js
@@ -4,3 +4,8 @@ const repoCount = 33; // Number
 // console.log(name + repoCount + " repositories"); // Concatenation (not recommended way outdated way)
 
 console.log(`${name} ${repoCount} repositories`); // Template Literal (recommended way modern way)
+
+
+// String Object
+const myName = new String("Mahar GM"); // String object (not recommended)
+console.log(myName);
