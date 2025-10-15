6a41f2a0733068e8640d4ef221df5e08a236d3d4
Mahar GM <144817379+github-mahar@users.noreply.github.com>
2025-10-15 21:25:36 +0500

    updated string.js

--- Lessons/05_strings.js
+++ Lessons/05_strings.js
@@ -11,3 +11,9 @@ const myName = new String("Mahar GM"); // String object (not recommended)
 console.log(myName);
 console.log(myName[0]); // Accessing first character
 console.log(myName.length); // Length of the string
+console.log(myName.toUpperCase()); // Convert to uppercase
+console.log(myName.toLowerCase()); // Convert to lowercase
+console.log(myName.charAt(0)); // Accessing first character using charAt method
+console.log(myName.indexOf("GM")); // Finding index of substring
+console.log(myName.lastIndexOf("a")); // Finding last index of substring
+console.log(myName.includes("Mahar")); // Checking if substring is present
