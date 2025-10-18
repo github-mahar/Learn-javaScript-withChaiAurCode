ff2cdd6d7ab104fb8ad6bdd47a281cfd1e99a014
Mahar GM <144817379+github-mahar@users.noreply.github.com>
2025-10-18 20:42:14 +0500

    before slice

--- Lessons/05_strings.js
+++ Lessons/05_strings.js
@@ -11,3 +11,11 @@ const myName = new String("Mahar GM"); // String object (not recommended)
 console.log(myName);
 console.log(myName[0]); // Accessing first character
 console.log(myName.length); // Length of the string
+
+// String Methods
+console.log(myName.toUpperCase()); // Convert to Uppercase
+console.log(myName.toLowerCase()); // Convert to Lowercase
+console.log(myName.includes("GM")); // Check if substring exists
+console.log(myName.startsWith("Mahar")); // Check if starts with substring
+console.log(myName.charAt(2)); // Character at index 2'))
+console.log(myName.indexOf("GM")); // Index of substring
