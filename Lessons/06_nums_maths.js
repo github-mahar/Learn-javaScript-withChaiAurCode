e24cf0c2f9ebcb83eb0db4578614dec2e98429f9
Mahar GM <144817379+github-mahar@users.noreply.github.com>
2025-10-21 23:07:56 +0500

    added nums_maths.js

---
+++ Lessons/06_nums_maths.js
@@ -0,0 +1,11 @@
+const score = 85; // number primitive
+console.log("Score:", score);
+
+const balance = new Number(1000);  // creating a number object
+console.log("Balance:", balance);
+
+
+// Using Number methods
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
