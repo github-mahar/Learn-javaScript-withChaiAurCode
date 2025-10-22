294a023af8bfdc30a8a845ea0aefa1cb04880dee
Mahar GM <144817379+github-mahar@users.noreply.github.com>
2025-10-22 21:45:24 +0500

    updated nums_maths.js

--- Lessons/06_nums_maths.js
+++ Lessons/06_nums_maths.js
@@ -8,4 +8,7 @@ console.log("Balance:", balance);
 // Using Number methods
 
 console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+console.log(balance.toFixed(2)); 
+console.log(Number.isInteger(score));
+console.log(Number.parseFloat("123.45abc")); // 123.45
+console.log(Number.parseInt("123abc")); // 123
