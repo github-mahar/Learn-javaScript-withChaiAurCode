00cd5a6697648e9dc0f8e8df673cc6ea3b6c1a10
Mahar GM <144817379+github-mahar@users.noreply.github.com>
2025-10-24 21:16:20 +0500

    updated nums and maths.js

--- Lessons/06_nums_maths.js
+++ Lessons/06_nums_maths.js
@@ -4,11 +4,9 @@ console.log("Score:", score);
 const balance = new Number(1000);  // creating a number object
 console.log("Balance:", balance);
 
-
 // Using Number methods
+console.log(balance.toString().length); // 4
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2)); 
+console.log(balance.toFixed(2)); // "1000.00"
+
 console.log(Number.isInteger(score));
-console.log(Number.parseFloat("123.45abc")); // 123.45
-console.log(Number.parseInt("123abc")); // 123
